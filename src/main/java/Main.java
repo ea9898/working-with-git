2315be664da803fc9a418984a4ec7df6337f1c4d
sevgeniy <ea9898@yandex.ru>
2024-08-20 13:12:54 +0300

    third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        print();
+    }
+
+    private static void print() {
+        System.out.println("Hello");
+    }
+
+    private static void printByeBye() {
+        System.out.println("Bye-bye");
     }
 }
