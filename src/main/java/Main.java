cb6cd1fae46694d5bafc4383c27cdb4b6f5ca478
sevgeniy <ea9898@yandex.ru>
2024-08-20 12:58:15 +0300

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        print();
+    }
+
+    private static void print() {
+        System.out.println("Hello");
     }
 }
